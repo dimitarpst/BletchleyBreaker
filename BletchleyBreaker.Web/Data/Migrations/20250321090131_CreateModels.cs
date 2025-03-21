1e366c5713e2d50587cf5eb9d50228ae967abd36
Damian179 <deymi2007@abv.bg>
2025-03-21 11:06:50 +0200

    fixed errors

---
+++ BletchleyBreaker.Web/Data/Migrations/20250321090131_CreateModels.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BletchleyBreaker.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GameSessions",
+                columns: table => new
+                {
+                    GameSessionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Player1Id = table.Column<int>(type: "int", nullable: false),
+                    Player2Id = table.Column<int>(type: "int", nullable: false),
+                    TargetCoordinates = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CurrentPlayerId = table.Column<int>(type: "int", nullable: false),
+                    IsGameOver = table.Column<bool>(type: "bit", nullable: false),
+                    AttemptsLeft = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameSessions", x => x.GameSessionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    PlayerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlayerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsComputer = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.PlayerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Guesses",
+                columns: table => new
+                {
+                    GuessId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GameSessionId = table.Column<int>(type: "int", nullable: false),
+                    PlayerId = table.Column<int>(type: "int", nullable: false),
+                    GuessedCoordinates = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CorrectNumbers = table.Column<int>(type: "int", nullable: false),
+                    CorrectPositions = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guesses", x => x.GuessId);
+                    table.ForeignKey(
+                        name: "FK_Guesses_GameSessions_GameSessionId",
+                        column: x => x.GameSessionId,
+                        principalTable: "GameSessions",
+                        principalColumn: "GameSessionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Guesses_GameSessionId",
+                table: "Guesses",
+                column: "GameSessionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Guesses");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "GameSessions");
+        }
+    }
+}
